241c5ac1380da2897f270d83af41a10b9771c56f
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-02-20 20:31:55 +0100

    Serialize gucci

--- objects/registry.py
+++ objects/registry.py
@@ -23,11 +23,11 @@ class Registry:
         for library in libraries:
             self.internal_registry += list(library.scan(self))
 
-    def serialize(self) -> str:
+    def serialize(self, sign_up_sorted_libraries: List[Library]) -> str:
         output = str(len(self.internal_registry.values()))
 
-        for library in self.internal_registry:
+        for library in sign_up_sorted_libraries:
             scanned_books = self.internal_registry[library]
-            output += f"{library.id} {len(scanned_books)}\n{' '.join([str(book.id) for book in scanned_books])}]"
+            output += f"{str(library.id)} {len(scanned_books)}\n{' '.join([str(book.id) for book in scanned_books])}"
 
         return output
