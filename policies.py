556a36bbec3ac633e8d3b5c24fd07ed0ea985b69
David Mendoza Martínez <david.mendoza.mnez@gmail.com>
2020-02-20 21:53:05 +0100

    d

--- policies.py
+++ policies.py
@@ -14,9 +14,9 @@ def capacity_per_sign_up_time(library: Library) -> int:
     return library.scan_capacity / (library.time or 1) * -1
 
 
-def boosted_capacity_per_sign_up_time(library: Library) -> int:
-    return library.scan_capacity * BOOSTING / (library.time or 1) * -1
-
-
 def highest_book_score(library: Library) -> int:
     return sum(b.score for b in library.books)
+
+
+def boosted_capacity_per_sign_up_time(library: Library) -> int:
+    return library.scan_capacity + highest_book_score(library) / (library.time or 1) * -1
