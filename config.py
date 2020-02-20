d5fb145b670da97435e9add725ca2d2dfe1cc8a7
Javier Luna molina <javierlunamolina@gmail.com>
2020-02-20 20:13:49 +0100

    Add input parsing

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 import multiprocessing
-from typing import Set
+from typing import List
 
-from objects.book import Book
+from objects.library import Library
 from objects.registry import Registry
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
@@ -10,12 +10,17 @@ SOLUTIONS = os.path.join(base_dir, 'solutions')
 INPUT = os.path.join(base_dir, 'input')
 
 N_THREADS = None
-INPUT_TYPE = str
+INPUT_TYPE = List[Library]
 OUTPUT_TYPE = str
 REGISTRY_TYPE = Registry
 
 INPUT_FILES = [
-
+    "a_example.txt",
+    "b_read_on.txt",
+    "c_incunabula.txt",
+    "d_tough_choices.txt",
+    "e_so_many_books.txt",
+    "f_libraries_of_the_world.txt"
 ]
 
 N_THREADS = N_THREADS if N_THREADS is not None else multiprocessing.cpu_count() - 1
--- script.py
+++ script.py
@@ -1,12 +1,34 @@
+from typing import List
+
 from config import INPUT_TYPE, OUTPUT_TYPE
+from objects.book import Book
+from objects.library import Library
 
 
 def do_solution(input: INPUT_TYPE) -> OUTPUT_TYPE:
     return ""
 
 
+def get_int_list(line: str) -> List[int]:
+    return [int(c) for c in line.split(' ')]
+
+
 def parse_input(input_content: str) -> INPUT_TYPE:
-    return input_content
+    lines = input_content.split('\n')
+    n_books, n_libraries, n_days = get_int_list(lines.pop(0))
+    book_scoring = get_int_list(lines.pop(0))
+
+    books = {i: Book(i, score) for i, score in enumerate(book_scoring)}
+    libraries = []
+
+    for library_id in range(n_libraries):
+        total_books, signup_time, scan_capacity = get_int_list(lines.pop(0))
+        libraries.append(Library(library_id,
+                                 {books[book_id] for book_id in get_int_list(lines.pop(0))},
+                                 signup_time,
+                                 scan_capacity))
+
+    return libraries
 
 
 def serialize_output(output: OUTPUT_TYPE) -> str:
